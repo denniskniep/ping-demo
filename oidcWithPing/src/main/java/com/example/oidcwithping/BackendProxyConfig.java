d5350f74effb818b2ee40b6961a8fa65124c7a06
Dennis Kniep <kniepdennis@gmail.com>
2021-01-31 20:48:52 +0100

    Fix NPE

--- oidcWithPing/src/main/java/com/example/oidcwithping/BackendProxyConfig.java
+++ oidcWithPing/src/main/java/com/example/oidcwithping/BackendProxyConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.io.Resource;
+import org.thymeleaf.util.StringUtils;
 
 import java.io.IOException;
 
@@ -15,11 +16,12 @@ public class BackendProxyConfig implements ApplicationListener<ApplicationPrepar
 
         String host = environment.getProperty("proxy.host");
         String port = environment.getProperty("proxy.port");
+        if(!StringUtils.isEmpty(host) && !StringUtils.isEmpty(port)){
+            System.setProperty("http.proxyHost", host);
+            System.setProperty("http.proxyPort", port);
 
-        System.setProperty("http.proxyHost", host);
-        System.setProperty("http.proxyPort", port);
-
-        System.setProperty("https.proxyHost", host);
-        System.setProperty("https.proxyPort", port);
+            System.setProperty("https.proxyHost", host);
+            System.setProperty("https.proxyPort", port);
+        }
     }
 }
